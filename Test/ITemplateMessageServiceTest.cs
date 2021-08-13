3528b04a553104bc0953d311f7f3dbd7f42d00cb
剑喜 叶 <JamesYe3980@outlook.com>
2021-08-13 11:14:19 +0800

    添加Test_GetTemplates测试

---
+++ Test/ITemplateMessageServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using EasyWeChat.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Test
+{
+    [TestClass]
+    public class ITemplateMessageServiceTest: BaseTest
+    {
+        [TestMethod]
+        public async Task Test_GetTemplates()
+        {
+            ITemplateMessageService templateMessageService = _sp.GetService<ITemplateMessageService>();
+            var token = await base.GetAccessToken();
+            var templates = await templateMessageService.GetTemplates(token);
+            Assert.IsNotNull(templates);
+
+        }
+
+    }
+}
